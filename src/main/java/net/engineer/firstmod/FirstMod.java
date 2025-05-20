7cb95c1c49df5406c74f0724119bd7287b7e9e4f
Techead139 <pranaye2006@gmail.com>
2025-05-20 16:55:49 -0400

    alexandrite block and raw_alexandrite block

--- src/main/java/net/engineer/firstmod/FirstMod.java
+++ src/main/java/net/engineer/firstmod/FirstMod.java
@@ -1,6 +1,7 @@
 package net.engineer.firstmod;
 
 import com.mojang.logging.LogUtils;
+import net.engineer.firstmod.block.ModBlocks;
 import net.engineer.firstmod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
@@ -34,6 +35,7 @@ public class FirstMod {
         MinecraftForge.EVENT_BUS.register(this);
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
 
 
@@ -54,6 +56,10 @@ public class FirstMod {
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
             event.accept(ModItems.ALEXANDRITE);
             event.accept(ModItems.RAW_ALEXANDRITE);
+        }
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS){
+            event.accept(ModBlocks.ALEXANDRITE_BLOCK);
+            event.accept(ModBlocks.RAW_ALEXANDRITE_BLOCK);
         }
 
     }
